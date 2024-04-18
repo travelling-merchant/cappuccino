8cb1412ab7328cb05dffdcd69e7023b019f7008e
birds-are-corrupted <69503678+birds-are-corrupted@users.noreply.github.com>
2024-04-18 19:56:46 +0200

    trasnfer to book and main

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -664,14 +664,22 @@ require("lazy").setup({
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"folke/tokyonight.nvim",
+		ops = {
+			colorscheme = "tokyonight-night",
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			--vim.cmd.colorscheme("tokyonight-night")
 			-- vim.cmd.colorscheme()
-
+			vim.opt.termguicolors = true
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
 		end,
